5d007ec6170fca052bdfab6623a61b3ce51b70d6
Saket goswami <110333651+saketgoswami09@users.noreply.github.com>
2026-04-05 10:52:14 +0530

    Remove unnecessary z-index from footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -13,7 +13,7 @@ const footerLinks = [
 
 export const Footer = () => {
   return (
-    <footer className="relative -z-10 overflow-x-clip">
+    <footer className="relative overflow-x-clip">
       <div className="absolute bottom-0 left-1/2 -z-10 h-[400px] w-[1600px] -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)]"></div>
 
       <div className="container">
